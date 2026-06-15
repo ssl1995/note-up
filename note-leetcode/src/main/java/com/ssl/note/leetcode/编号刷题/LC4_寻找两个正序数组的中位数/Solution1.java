f12a0bbf80bb7ca102bde348e89109743a54c789
songshenglin <1363912005@163.com>
2026-06-15 12:17:59 +0800

    LC4_寻找两个正序数组的中位数-1

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC4_寻找两个正序数组的中位数/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC4_寻找两个正序数组的中位数/Solution1.java
@@ -15,33 +15,51 @@ public class Solution1 {
     // m是短数组长度，n是长数组长度
     int m = nums1.length;
     int n = nums2.length;
-    int left = 0, right = m;
+    int nums1Left = 0, nums1Right = m;
 
-    while (left <= right) {
-      int i = (left + right) / 2;        // nums1 的划分点
-      int j = (m + n + 1) / 2 - i;       // nums2 的划分点
+    // num1=[4],nums2=[1,2,3,5],m=1,n=4
+    while (nums1Left <= nums1Right) {
+      // nums1:  [ 1,  3,  5 | 7,  9 ]
+      // nums2:  [ 2,  4,  6 | 8, 10 ]
+      //            L 区域      R 区域
+      // i=0
+      int i = (nums1Left + nums1Right) / 2;
+      // j=3
+      // 总长为奇数时，多一个
+      int j = (m + n + 1) / 2 - i;
 
+      // nums1LeftMax=MIN_VALUE、nums1RightMin=4
       int nums1LeftMax = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
       int nums1RightMin = (i == m) ? Integer.MAX_VALUE : nums1[i];
+      // nums2LeftMax=3、nums2RightMin=5
       int nums2LeftMax = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
       int nums2RightMin = (j == n) ? Integer.MAX_VALUE : nums2[j];
 
+      // max(nums1[i-1], nums2[j-1]) <= min(nums1[i], nums2[j])
+      // 由于nums1[i-1]<nums1[i]和 nums2[j-1]<nums2[j]天然成立，所以只需要交叉比较就行
       if (nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
         // 划分正确
         if ((m + n) % 2 == 0) {
-          return (Math.max(nums1LeftMax, nums2LeftMax)
-              + Math.min(nums1RightMin, nums2RightMin)) / 2.0;
+          return (Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin)) / 2d;
         } else {
           return Math.max(nums1LeftMax, nums2LeftMax);
         }
-      } else if (nums1LeftMax > nums2RightMin) {
-        // nums1 左边太大，往左缩
-        right = i - 1;
+      }// 以下2个分支确定交叉点i和j的位置
+      else if (nums1LeftMax > nums2RightMin) {
+        // nums1左边元素没有过大，把它从左边挪到右边，i减少
+        nums1Right = i - 1;
       } else {
-        // nums1 左边太小，往右扩
-        left = i + 1;
+        // num2左边元素没有过大=nums1左边元素太小，i增大
+        nums1Left = i + 1;
       }
     }
-    return 0.0;
+    return -1d;
+  }
+
+  public static void main(String[] args) {
+    Solution1 solution2 = new Solution1();
+    int[] nums1 = {1, 2};
+    int[] nums2 = {3, 4, 5};
+    System.out.println(solution2.findMedianSortedArrays(nums1, nums2));
   }
 }
