94e54af114fccf3ef518ab6c8cebb88cb2692bbd
songshenglin <1363912005@163.com>
2026-06-22 09:13:52 +0800

    LC62_不同路径

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC62_不同路径/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC62_不同路径/Solution.java
@@ -16,6 +16,7 @@ public class Solution {
    */
   public int uniquePaths(int m, int n) {
     int[][] dp = new int[m][n];
+    // 初始化第一列和第一行，值=1
     for (int i = 0; i < m; i++) {
       dp[i][0] = 1;
     }
