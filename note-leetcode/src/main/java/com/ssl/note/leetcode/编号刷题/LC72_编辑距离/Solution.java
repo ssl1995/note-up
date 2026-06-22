850e3e86c25ada6e527bea4eafdf0fca727c81b6
songshenglin <1363912005@163.com>
2026-06-22 17:29:19 +0800

    LC72_编辑距离

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC72_编辑距离/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC72_编辑距离/Solution.java
@@ -21,11 +21,13 @@ public class Solution {
     int[][] dp = new int[m + 1][n + 1];
     // 初始化
     dp[0][0] = 0;
+    // word2 为空，只能把 word1 的字符一个个删除
     for (int i = 0; i < m + 1; i++) {
-      dp[i][0] = i;// 删除i次
+      dp[i][0] = i;
     }
+    // word1 为空，只能往 word2 一个个插入
     for (int j = 0; j < n + 1; j++) {
-      dp[0][j] = j;// 插入j次
+      dp[0][j] = j;
     }
     // 动态转移
     for (int i = 1; i < m + 1; i++) {
@@ -35,9 +37,13 @@ public class Solution {
           dp[i][j] = dp[i - 1][j - 1];
           continue;
         }
+        // 删除：dp[i-1][j] + 1
+        // 新增：dp[i][j-1] + 1
+        // 替换：dp[i-1][j-1] + 1
         // 字母不相同：（删除、新增、替换取最小） + 1
-        dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]))
-            + 1;
+        int min1 = Math.min(dp[i][j - 1], dp[i - 1][j - 1]);
+        int min = Math.min(min1, dp[i - 1][j - 1]);
+        dp[i][j] = min + 1;
       }
     }
 
--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC72_编辑距离/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC72_编辑距离/Solution1.java
@@ -8,8 +8,9 @@ package com.ssl.note.leetcode.编号刷题.LC72_编辑距离;
 public class Solution1 {
 
   /**
-   * 编辑距离
-   * 返回将word1转换成word2所使用的最少操作数
+   * 编辑距离 - 一维数组空间优化版本
+   * 返回将 word1 转换成 word2 所使用的最少操作数
+   *
    * 输入：word1 = "intention", word2 = "execution"
    * 输出：5
    * 解释：
@@ -19,39 +20,61 @@ public class Solution1 {
    * exention -> exection (将 'n' 替换为 'c')
    * exection -> execution (插入 'u')
    */
-  // 一维数组空间优化
   public int minDistance(String word1, String word2) {
+    // 技巧：让 word1 是较长的字符串，word2 是较短的字符串
+    // 这样 dp 数组长度取 min(m, n)，空间更优
     if (word1.length() < word2.length()) {
       return minDistance(word2, word1);
     }
-    int m = word1.length();
-    int n = word2.length();
-    int[] dp = new int[n + 1];
 
+    int m = word1.length();  // 较长字符串
+    int n = word2.length();  // 较短字符串
+
+    // dp[j]：word1[0..i-1] 和 word2[0..j-1] 在当前行 i 下的最少编辑距离
+    int[] dp = new int[n + 1];
+    // 初始化 dp 数组：当 word1 为空串时
     for (int j = 1; j <= n; j++) {
       dp[j] = j;
     }
 
+    // 遍历 word1 的每个字符，逐行更新 dp 数组
     for (int i = 1; i <= m; i++) {
-      int prev = dp[0];  // 左上角的值
-      dp[0] = i;         // 第一列更新
+      // prev=dp[i-1][j-1]，也就是"左上角"的值
+      int prev = dp[0];
+      // 特殊处理：更新 dp[0]：当 word2 为空串时
+      // 需要把 word1[0..i-1] 全部删除，操作数就是 i
+      dp[0] = i;
+
       for (int j = 1; j <= n; j++) {
+        // temp=dp[i-1][j]=二维表中的"上方"
         int temp = dp[j];
+
         if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+          // 当前字符相等：不需要任何操作
+          // 直接继承左上角 dp[i-1][j-1] 的值，也就是 prev
           dp[j] = prev;
         } else {
+          // 当前字符不相等：三种操作取最小值 + 1
+          // dp[j]      = 二维表中的 dp[i-1][j]，表示"删除"操作
+          // dp[j - 1]  = 二维表中的 dp[i][j-1]，表示"插入"操作
+          // prev       = 二维表中的 dp[i-1][j-1]，表示"替换"操作
           dp[j] = Math.min(Math.min(dp[j], dp[j - 1]), prev) + 1;
         }
+
+        // 更新 prev：当前 temp 是 dp[i-1][j]
+        // 对于下一列 j+1 来说，它就是左上角 dp[i-1][j]
         prev = temp;
       }
     }
+
+    // dp[n] 最终表示 word1[0..m-1] 和 word2[0..n-1] 的最少编辑距离
     return dp[n];
   }
 
   public static void main(String[] args) {
-    Solution1 solution = new Solution1();
+    Solution1 solution1 = new Solution1();
     String w1 = "intention";
     String w2 = "execution";
-    System.out.println(solution.minDistance(w1, w2));
+    System.out.println(solution1.minDistance(w1, w2));
   }
 }
