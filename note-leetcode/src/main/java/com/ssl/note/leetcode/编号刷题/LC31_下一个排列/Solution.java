c313bb7dad7d18407943fb9b4775905a6882befc
songshenglin <1363912005@163.com>
2026-06-22 22:30:54 +0800

    LC31_下一个排列

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC31_下一个排列/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC31_下一个排列/Solution.java
@@ -9,11 +9,11 @@ public class Solution {
    * 算法核心：找到比当前排列大的最小排列
    * 步骤解析：
    * 1. 找下降点：从右往左找第一个i，使得nums[i] < nums[i+1]
-   *    - 意义：i是第一个可以调整的位置，右边的元素已降序，无法再增大
+   * - 意义：i是第一个可以调整的位置，右边的元素已降序，无法再增大
    * 2. 交换大值：从右往左找第一个j，使得nums[j] > nums[i]，交换两者
-   *    - 意义：找到最小的可能的更大数来替换i位置，保证是"刚好大一点"
+   * - 意义：找到最小的可能的更大数来替换i位置，保证是"刚好大一点"
    * 3. 反转剩余：反转i+1到末尾的元素
-   *    - 意义：交换后右边仍为降序，反转后变为升序，得到最小的可能排列
+   * - 意义：交换后右边仍为降序，反转后变为升序，得到最小的可能排列
    * 示例：[4,5,2,6,3,1]
    * 1. 找下降点：i=2（2 < 6）
    * 2. 找交换点：j=4（3 > 2），交换后 [4,5,3,6,2,1]
@@ -27,17 +27,16 @@ public class Solution {
     if (nums.length < 2) {
       return;
     }
-    
-    // 步骤1：从右往左找下降点i
+    // 从右找下坡，从右找大数，交换再反转
+    // 步骤1：尽量不动高位，高位动了变化太大,从右往左找下降点i
     // 下降点定义：第一个满足nums[i] < nums[i+1]的位置
-    // 从倒数第二个元素开始往左找
     int i = nums.length - 2;
     while (i >= 0 && nums[i] >= nums[i + 1]) {
-      // 当前元素大于等于下一个元素，继续往左找
       i--;
     }
-    
+
     // 步骤2：如果找到下降点i，从右往左找第一个比nums[i]大的元素j
+    // i>=0必须加上，因为可能i=-1说明是最大排列数，交换后变成最小的排列
     if (i >= 0) {
       int j = nums.length - 1;
       while (j >= 0 && nums[j] <= nums[i]) {
@@ -47,7 +46,7 @@ public class Solution {
       // 交换nums[i]和nums[j]，增大i位置的值
       swap(nums, i, j);
     }
-    
+
     // 步骤3：反转i+1到末尾的元素
     // 因为交换后，i+1到末尾仍为降序，反转后变为升序
     // 这样可以得到最小的可能的更大排列
@@ -83,22 +82,22 @@ public class Solution {
 
   public static void main(String[] args) {
     Solution solution = new Solution();
-    
+
     // 测试用例1：正常情况
     int[] nums1 = {4, 5, 2, 6, 3, 1};
     solution.nextPermutation(nums1);
     System.out.println("Test 1: " + Arrays.toString(nums1)); // 期望：[4,5,3,1,2,6]
-    
+
     // 测试用例2：最大排列
     int[] nums2 = {3, 2, 1};
     solution.nextPermutation(nums2);
     System.out.println("Test 2: " + Arrays.toString(nums2)); // 期望：[1,2,3]
-    
+
     // 测试用例3：重复元素
     int[] nums3 = {1, 1, 5};
     solution.nextPermutation(nums3);
     System.out.println("Test 3: " + Arrays.toString(nums3)); // 期望：[1,5,1]
-    
+
     // 测试用例4：最小排列
     int[] nums4 = {1, 2, 3};
     solution.nextPermutation(nums4);
