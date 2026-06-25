d9ee1e527c2e1f628d33c6b50e820a86b20d6135
songshenglin <1363912005@163.com>
2026-06-25 08:30:02 +0800

    LC15_三数之和

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC15_三数之和/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC15_三数之和/Solution1.java
@@ -9,6 +9,7 @@ public class Solution1 {
   /**
    * 三数之和
    * 是否存在3个数使得a+b+c=0
+   * 注意：答案中不可以包含重复的三元组
    * 输入：nums = [-1,0,1,2,-1,-4]
    * 输出：[[-1,-1,2],[-1,0,1]]
    */
@@ -21,11 +22,11 @@ public class Solution1 {
     Arrays.sort(nums);
     int n = nums.length;
     for (int i = 0; i < n - 2; i++) {
-      // 2.剪枝：最小值大于0，不可能有解
+      // 2.加速1：剪枝：最小值大于0，不可能有解
       if (nums[i] > 0) {
         break;
       }
-      // 3.去重：跳过重复的第一个数
+      // 3.加速2：去重：跳过重复的第一个数
       if (i > 0 && nums[i] == nums[i - 1]) {
         continue;
       }
@@ -41,8 +42,9 @@ public class Solution1 {
         } else {
           // 5.找到答案
           res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-          // 6.跳过重复值：例如；[0,0,0,0] 固定第一个0，后续3个0返回的3元组从数字上来看是会重复的
           // 要求：答案中不可以包含重复的三元组。
+          // 6.非加速，但需要跳过重复值
+          // 例如；[0,0,0,0] 固定第一个0后，双指针返回第一个(0,0,0)后，不要再重复返回了
           while (left < right && nums[left] == nums[left + 1]) {
             left++;
           }
