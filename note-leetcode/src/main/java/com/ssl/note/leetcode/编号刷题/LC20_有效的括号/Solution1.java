09b616a9225d01bee897e1305e7283f3dcf803cf
songshenglin <1363912005@163.com>
2026-08-25 23:24:28 +0800

    test7

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC20_有效的括号/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC20_有效的括号/Solution1.java
@@ -10,7 +10,7 @@ import java.util.Map;
  * @date 2022/1/20 11:29 PM
  * @description
  */
-public class Solution {
+public class Solution1 {
 
   /**
    * 有效的括号
@@ -27,31 +27,25 @@ public class Solution {
     map.put('[', ']');
     map.put('{', '}');
 
-    for (char c : s.toCharArray()) {
-      // 第一次入栈
-      if (stack.isEmpty()) {
-        stack.push(c);
-        continue;
-      }
-      // 栈不为空，就判断栈顶和待插入字符关系
-      char peek = stack.peek();
-      // 只有左边的符号能入栈
-      if (!map.containsKey(peek)) {
-        return false;
-      }
 
-      if (c == map.get(peek)) {
-        stack.pop();
-      } else {
+    for (char c : s.toCharArray()) {
+      // 入栈：左括号才能入栈
+      if (map.containsKey(c)) {
         stack.push(c);
+      } else {
+        // 否则就需要出栈
+        // 栈空 或者 待插入不匹配栈顶
+        if (stack.isEmpty() || c != map.get(stack.peek())) {
+          return false;
+        }
+        stack.pop();
       }
     }
-
     return stack.isEmpty();
   }
 
   public static void main(String[] args) {
-    Solution solution = new Solution();
+    Solution1 solution = new Solution1();
     System.out.println(solution.isValid("(]"));
   }
 }
