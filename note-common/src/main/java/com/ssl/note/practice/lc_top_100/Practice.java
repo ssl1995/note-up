7a8e85399e2626c1e8a68819489862f5bc899691
songshenglin <872912679@qq.com>
2026-08-26 16:38:49 +0800

    完成

--- note-common/src/main/java/com/ssl/note/practice/lc_top_100/Practice.java
+++ note-common/src/main/java/com/ssl/note/practice/lc_top_100/Practice.java
@@ -1,91 +1,56 @@
 package com.ssl.note.practice.lc_top_100;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Practice {
 
-  public String decodeString(String s) {
-    if (s == null || s.length() == 0) {
-      return "";
+  public int[] topKFrequent(int[] nums, int k) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
     }
-    Deque<String> letterStack = new ArrayDeque<>();
-    Deque<Integer> numberStack = new ArrayDeque<>();
 
-    int num = 0;
-    StringBuilder res = new StringBuilder();
-    for (char c : s.toCharArray()) {
-      if (c >= '1' && c <= '9') {
-        num = num * 10 + c - '0';
-      } else if (c >= 'a' && c <= 'z') {
-        res.append(c);
-      } else if (c == '[') {
-        letterStack.push(res.toString());
-        numberStack.push(num);
-        res = new StringBuilder();
-        num = 0;
-      } else {
-        int count = numberStack.isEmpty() ? 0 : numberStack.pop();
+    int n = nums.length;
+    List<List<Integer>> buckets = new ArrayList<>();
+    // 频率是下标[1,n]
+    for (int i = 0; i <= n; i++) {
+      buckets.add(new ArrayList<>());
+    }
 
-        StringBuilder temp = new StringBuilder();
-        while (count-- > 0) {
-          temp.append(res);
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      buckets.get(entry.getValue()).add(entry.getKey());
+    }
+
+    int[] res = new int[k];
+    int index = 0;
+    for (int i = n; i >= 0; i--) {
+      if (index >= k) {
+        break;
+      }
+      if (buckets.get(i).isEmpty()) {
+        continue;
+      }
+      List<Integer> sameCounts = buckets.get(i);
+      for (int num : sameCounts) {
+        if (index >= k) {
+          break;
         }
-
-        res = new StringBuilder(letterStack.pop() + temp);
+        res[index++] = num;
       }
     }
 
-    return res.toString();
+    return res;
   }
 
-  public String decodeString1(String s) {
-    if (s.isEmpty()) {
-      return "";
-    }
-
-    Deque<Integer> numsStack = new ArrayDeque<>();
-    Deque<String> letterStack = new ArrayDeque<>();
-
-    int multi = 0;
-    StringBuilder res = new StringBuilder();
-    char[] cs = s.toCharArray();
-    for (char c : cs) {
-      // 数字，数字可能超过十位
-      if (c >= '0' && c <= '9') {
-        multi = multi * 10 + (c - '0');
-      } else if (c == '[') {
-        // 左括号，压入数字栈和字母栈
-        numsStack.push(multi);
-        multi = 0;
-
-        letterStack.push(res.toString());
-        res = new StringBuilder();
-      } else if (c == ']') {
-        // 右括号，弹出数字栈和字母栈，拼接字符串
-        int count = numsStack.pop();
-
-        StringBuilder temp = new StringBuilder();
-        while (count-- > 0) {
-          temp.append(res);
-        }
-
-        // 与弹出的字母栈拼接
-        res = new StringBuilder(letterStack.pop() + temp);
-      } else {
-        // 字母，保存当前res
-        res.append(c);
-      }
-    }
-
-    return res.toString();
-  }
 
   public static void main(String[] args) {
     Practice practice = new Practice();
     String s = "10[abc]";
-    System.out.println(practice.decodeString1(s));
-    System.out.println(practice.decodeString(s));
+
   }
 
 }
--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC295_数据流中位数/MedianFinder.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC295_数据流中位数/MedianFinder.java
@@ -4,21 +4,11 @@ import java.util.PriorityQueue;
 
 public class MedianFinder {
 
-  /**
-   * 中位数是有序整数列表中的中间值。如果列表的大小是偶数，则没有中间值，中位数是两个中间值的平均值。
-   * 例如 arr = [2,3,4] 的中位数是 3 。
-   * 例如 arr = [2,3] 的中位数是 (2 + 3) / 2 = 2.5 。
-   * 实现 MedianFinder 类:
-   * MedianFinder() 初始化 MedianFinder 对象。
-   * void addNum(int num) 将数据流中的整数 num 添加到数据结构中。
-   * double findMedian() 返回到目前为止所有元素的中位数。与实际答案相差 10-5 以内的答案将被接受。
-   */
   private PriorityQueue<Integer> maxHeap;// 大根堆：小的一半放这里，堆顶是小的一半里的最大值
   private PriorityQueue<Integer> minHeap;// 小根堆：大的一半放这里，堆顶是大的一半里的最小值
 
   public MedianFinder() {
     maxHeap = new PriorityQueue<>((a, b) -> b - a);
-//    maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
     minHeap = new PriorityQueue<>();
   }
 
@@ -29,26 +19,23 @@ public class MedianFinder {
     } else {
       minHeap.offer(num);
     }
-    // 平衡堆：大根堆 == 小根堆，或者大根堆多 1
-    balance1();
-//    balance2();
+    // 平衡堆
+    balance();
   }
 
-  private void balance1() {
+  private void balance() {
+    /**
+     * 合法状态：
+     * 奇数：中位数在中间，一堆比另一堆多一个，规定maxHeap比minHeap多一个是合法状态
+     * 偶数：中位数在中间两边，两个堆数量相等合法
+     */
     if (maxHeap.size() == minHeap.size() || maxHeap.size() == minHeap.size() + 1) {
       return;
     }
+    // 长度差超过1，谁大就移动谁
     if (maxHeap.size() > minHeap.size() + 1) {
       minHeap.offer(maxHeap.poll());
     } else {
-      maxHeap.offer(minHeap.poll());
-    }
-  }
-
-  private void balance2() {
-    if (maxHeap.size() > minHeap.size() + 1) {
-      minHeap.offer(maxHeap.poll());
-    } else if (maxHeap.size() < minHeap.size()) {
       maxHeap.offer(minHeap.poll());
     }
   }
@@ -59,14 +46,5 @@ public class MedianFinder {
     } else {
       return (double) maxHeap.peek();
     }
-  }
-
-  public static void main(String[] args) {
-    MedianFinder medianFinder = new MedianFinder();
-    medianFinder.addNum(1);
-    medianFinder.addNum(2);
-    System.out.println(medianFinder.findMedian());
-    medianFinder.addNum(3);
-    System.out.println(medianFinder.findMedian());
   }
 }
